d361070f923fa3005e637d7777aa7f02d9ffba74
Cory Buecker <email@corybuecker.com>
2023-04-30 09:37:46 -0500

    add react router as a bundled ESM

--- app/frontend/index.tsx
+++ app/frontend/index.tsx
@@ -1,6 +1,20 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-const mainElement = document.getElementsByTagName('main')
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
+import Main from './main.js'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+  },
+])
+const mainElement = document.getElementsByTagName('main')
 const root = createRoot(mainElement[0])
-root.render(<div>tssssest</div>)
+
+root.render(
+  <>
+    <RouterProvider router={router} />
+  </>
+)
