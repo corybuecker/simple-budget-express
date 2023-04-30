8db5d78d1920edcc8b8a9cdd88c5be7b11ecf959
Cory Buecker <email@corybuecker.com>
2023-04-30 14:28:01 -0500

    setup initial routes and error handling
    install tailwindcss

--- app/frontend/index.tsx
+++ app/frontend/index.tsx
@@ -1,20 +1,61 @@
-import React from 'react'
+import ErrorPage from './errors'
+import Main, { Account } from './main'
+import React, { StrictMode } from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-import Main from './main.js'
+const mainElements = document.getElementsByTagName('main')
+const mainElement = mainElements.item(0)
+
+const rootLoader = async () => {
+  const rawResponse: Response = await fetch('/api/accounts')
+  return (await rawResponse.json()) as Account[]
+}
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Main></Main>,
+    element: <Main />,
+    loader: rootLoader,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/accounts',
+    element: <Main />,
+    loader: rootLoader,
+    children: [],
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/savings',
+    element: <Main />,
+    loader: rootLoader,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/goals',
+    element: <Main />,
+    loader: rootLoader,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/reports',
+    element: <Main />,
+    loader: rootLoader,
+    errorElement: <ErrorPage />,
   },
 ])
-const mainElement = document.getElementsByTagName('main')
-const root = createRoot(mainElement[0])
 
-root.render(
-  <>
-    <RouterProvider router={router} />
-  </>
-)
+const Router = () => {
+  return
+}
+
+export default Router
+
+if (mainElement) {
+  createRoot(mainElement).render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  )
+}
