b8d05cbb838eb2efd0692e5ce07a931430d0d80a
Cory Buecker <email@corybuecker.com>
2023-05-03 07:49:22 -0500

    re-organize the imports

--- app/frontend/main.tsx
+++ app/frontend/main.tsx
@@ -1,20 +1,12 @@
 import React from 'react'
-import { Link, useLoaderData } from 'react-router-dom'
-import Nav from './nav'
-
-export type Account = {
-  id: number
-}
+import { Outlet } from 'react-router-dom'
+import Nav from './nav.js'
 
 const Main = () => {
-  const data = useLoaderData() as Account[]
-
   return (
     <>
       <Nav></Nav>
-      <h1>tewsst</h1>
-      <Link to="/accounts">Accounts</Link>
-      <>{data[0].id}</>
+      <Outlet />
     </>
   )
 }
---
+++ app/frontend/routes/accounts.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+export const Accounts = () => {
+  return <div>test</div>
+}
