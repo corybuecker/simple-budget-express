8872658b1a8354b5dc72b14d6c443c6aa733a64e
Cory Buecker <email@corybuecker.com>
2023-05-31 08:20:55 -0500

    add test coverage

--- src/controllers/accounts.ts
+++ src/controllers/accounts.ts
@@ -2,56 +2,54 @@ import { FastifyInstance, FastifyRequest } from 'fastify'
 import AuthenticatedRouteFactory from './authenticated_route_factory'
 import { User } from '../models/user'
 import { Account } from '../models/account'
+import { AccountValidator } from '../frontend/entities/accounts'
+
+export type AccountsControllerRequestTypes = {
+  Body: Account
+  Params: { id: string }
+}
 
 const registerAccountsController = (application: FastifyInstance) => {
   const getAccounts = new AuthenticatedRouteFactory<Account[]>({
     url: '/api/accounts',
     method: 'GET',
     handler: async (request: FastifyRequest, _) => {
-      const user = await User.findByPk(request.locals?.user.id, {
-        include: 'accounts',
-      })
-
-      return user?.accounts ?? []
+      return request.locals.user.accounts ?? []
     },
+    additionalRelationsForUser: ['accounts'],
   })
 
-  const createAccount = new AuthenticatedRouteFactory<Account, Account>({
+  const createAccount = new AuthenticatedRouteFactory<Account | Error>({
     url: '/api/accounts',
     method: 'POST',
     handler: async (request: FastifyRequest<{ Body: Account }>, _) => {
-      const user = await User.findOne({
-        where: { id: request.locals?.user.id },
-        rejectOnEmpty: true,
-      })
+      const accountValidator = new AccountValidator(request.body)
 
-      return user?.$create<Account>('account', request.body)
+      if ((await accountValidator.validate()).length > 0) {
+        throw Error('account is not valid')
+      }
+
+      return request.locals.user.$create<Account>('account', request.body)
     },
+    additionalRelationsForUser: ['accounts'],
   })
 
-  const getAccount = new AuthenticatedRouteFactory<
-    Account,
-    undefined,
-    { id: string }
-  >({
+  const getAccount = new AuthenticatedRouteFactory<Account>({
     url: '/api/accounts/:id',
     method: 'GET',
     handler: async (request: FastifyRequest<{ Params: { id: string } }>, _) => {
       const user = await User.findOne({
-        where: { id: request.locals?.user.id },
+        where: { id: request.locals.user?.id },
         rejectOnEmpty: true,
         include: ['accounts'],
       })
 
-      return user?.accounts.filter((a) => a.id === request.params.id)[0]
+      return user.accounts.filter((a) => a.id === request.params.id)[0]
     },
+    additionalRelationsForUser: ['accounts'],
   })
 
-  const patchAccount = new AuthenticatedRouteFactory<
-    Account,
-    Account,
-    { id: string }
-  >({
+  const patchAccount = new AuthenticatedRouteFactory<Account>({
     url: '/api/accounts/:id',
     method: 'PUT',
     handler: async (
@@ -61,13 +59,9 @@ const registerAccountsController = (application: FastifyInstance) => {
       }>,
       _
     ) => {
-      const user = await User.findOne({
-        where: { id: request.locals?.user.id },
-        rejectOnEmpty: true,
-        include: ['accounts'],
-      })
-
-      const account = user.accounts.filter((a) => a.id === request.params.id)[0]
+      const account = request.locals.user.accounts.filter(
+        (a) => a.id === request.params.id
+      )[0]
 
       account.name = request.body.name
       account.amount = request.body.amount
@@ -76,6 +70,7 @@ const registerAccountsController = (application: FastifyInstance) => {
 
       return account
     },
+    additionalRelationsForUser: ['accounts'],
   })
 
   application.route(getAccounts.authenticated())
--- src/models/user.ts
+++ src/models/user.ts
@@ -24,7 +24,7 @@ export class User extends Model {
   @Column({ type: DataType.STRING, allowNull: false })
   declare email: string
 
-  @HasMany(() => Account)
+  @HasMany(() => Account, { onDelete: 'CASCADE' })
   declare accounts: Account[]
 
   @CreatedAt
